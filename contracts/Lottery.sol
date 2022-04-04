2bb81c566652ee334c6f43db8a2a1bf750c531cf
haroon <91939783+haroonalbar@users.noreply.github.com>
2022-04-04 22:32:28 +0530

    get entrance fee function in contract

---
+++ contracts/Lottery.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract Lottery{
+    address payable[] public players;
+    uint256 usdEntryFees;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFees = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+    }
+
+    function enter() public payable{
+        //min 50 usd
+        players.push(msg.sender);
+    }
+    function getEntranceFee() public view returns(uint256) {
+
+        (,int price, ,,)= ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price)*10**10;//made it equlavalnt to wei
+
+        uint256 costToEnter = (usdEntryFees * 10 ** 18)/adjustedPrice;
+        return costToEnter;
+
+    }
+    function startLottery() public {}
+    function endLottery() public {}
+}
