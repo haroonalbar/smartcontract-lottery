d1eb3b13bfbb2700d5e5f5859c2a2b6524d465df
haroon <91939783+haroonalbar@users.noreply.github.com>
2022-04-09 11:10:15 +0530

    update helpful scripts

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,2 +1,14 @@
+from brownie import accounts, config, network
+
+FORKED_LOCAL_ENVITONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCAHIN_ENVIRONMENT = ["development", "dog"]
+
+
 def get_account():
-    pass
+    if (
+        network.show_active() in LOCAL_BLOCKCAHIN_ENVIRONMENT
+        or network.show_active() in FORKED_LOCAL_ENVITONMENTS
+    ):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
